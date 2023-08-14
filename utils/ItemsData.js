c57249d9699cf87444c5f5f08ddb3c54f8c9a9d6
AndreySaveliev <101524084+AndreySaveliev@users.noreply.github.com>
2023-08-14 07:44:31 +0300

    itemdata

---
+++ utils/ItemsData.js
@@ -0,0 +1,44 @@
+export const itemsData = [
+  {
+    name: 'Футболка UZcotton мужская',
+    specs: ['Цвет: белый', 'Размер: 56'],
+    seller: 'Коледино WB',
+    org: 'OOO Вайлдберриз',
+    img: './assets/tshirt.png',
+    currentQuantity: 1,
+    maxQuantity: 2,
+    priveWithOutSale: 1051,
+    currentPrice: 522,
+    currency: 'сом',
+    available_shipping: ['5-6 февраля'],
+    available_shipping_quantity: []
+  },
+  {
+    name: 'Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe',
+    specs: ['Цвет: прозрачный'],
+    seller: 'Коледино WB',
+    org: 'OOO Вайлдберриз',
+    img: './assets/case.png',
+    currentQuantity: 200,
+    maxQuantity: null,
+    priveWithOutSale: 11500,
+    currentPrice: 10500,
+    currency: 'сом',
+    available_shipping: ['5-6 февраля', '7-8 февраля'],
+    available_shipping_quantity: [184, 16]
+  },
+  {
+    name: 'Карандаши цветные Faber-Castell "Замок", набор 24 цвета, заточенные,шестигранные,Faber-Castell',
+    specs: [],
+    seller: 'Коледино WB',
+    org: 'OOO Вайлдберриз',
+    img: './assets/pencils.png',
+    currentQuantity: 2,
+    maxQuantity: 2,
+    priveWithOutSale: 475,
+    currentPrice: 247,
+    currency: 'сом',
+    available_shipping: ['5-6 февраля'],
+    available_shipping_quantity: []
+  },
+];
